4356661499d36f82f063cceb4b007e13ac0633e8
mayankshoree1992 <139949639+mayankshoree1992@users.noreply.github.com>
2023-07-25 13:24:33 +0530

    Add files via upload

---
+++ GlobalException/GlobalException/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace GlobalException.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex, ex.Message);
+                await HandleExceptionAsync(httpContext, ex, _logger);
+            }
+        }
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex, ILogger _logger)
+        {
+            var serverError = HttpStatusCode.InternalServerError;
+            var output = JsonSerializer.Serialize(new { error = "The Error occured" });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)serverError;
+
+            string pathName = "GlobalException\\log.txt";// Put your file location here 
+            var date = DateTime.Now;
+            if (!File.Exists(pathName))
+            {
+                File.Create(pathName);
+                TextWriter tw = File.AppendText(pathName);
+                tw.WriteLine(date.ToString() + ' ' + ex.Message);
+                tw.Close();
+            }
+            else if (File.Exists(pathName))
+            {
+                TextWriter tw = File.AppendText(pathName);
+                tw.WriteLine(date.ToString() + ' ' + ex.Message);
+                tw.Close();
+            }
+
+            return context.Response.WriteAsync(output);
+        }
+    }
+}
---
+++ GlobalException/GlobalException/Program.cs
@@ -0,0 +1,29 @@
+using GlobalException.Middleware;
+using Serilog;
+using Serilog.Formatting.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
